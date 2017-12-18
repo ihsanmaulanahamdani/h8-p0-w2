eb6bf56dfe262ace407ac31551a536e4985fa3f8
ihsanmaulanahamdani <ihsanmaulana5@gmail.com>
2017-12-18 20:22:08 +0700

    excercise-week2-day1

---
+++ exercise-2.js
@@ -0,0 +1,28 @@
+//Contoh input 1
+var nama = 'Mikae';
+var peran = 'Ksatria';
+
+
+if (peran === '') {
+  console.log('Halo ' + nama + ',' + ' Pilih peranmu untuk memulai game!');
+} else {
+  if (peran === 'Ksatria') {
+    console.log('Selamat datang di Dunia Proxytia, ' + nama);
+    console.log(
+      'Halo ' + peran + ' ' + nama + ', kamu dapat menyerang dengan senjatamu!'
+    );
+  } else {
+    if (peran === 'Tabib') {
+      console.log('Selamat datang di Dunia Proxytia, ' + nama);
+      console.log(
+        'Halo ' + peran + ' ' + nama + ',' + ' kamu akan membantu temanmu yang terluka.'
+      );
+    } else {
+      if (peran === 'Penyihir') {
+        console.log('Selamat datang di Dunia Proxytia, ' + nama);
+        console.log(
+          'Halo ' + peran + ' ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu!');
+      }
+    }
+  }
+}
