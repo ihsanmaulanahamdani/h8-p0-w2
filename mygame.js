ba92c0bc71f6136a1d28c25fc008ec7bf40eb109
ihsanmaulanahamdani <ihsanmaulana5@gmail.com>
2017-12-25 13:52:37 +0700

    exercise-week2-day5

--- mygame.js
+++ mygame.js
@@ -1,36 +1,38 @@
 //Nama Game: Cars Racing Drag
-var namaPlayer = 'Ihsan';
+var namaPlayer = 'ihsan';
 var tipeMobil = ['BMW', 'Lamborghini', 'Ferrari', 'Tesla', 'Koenigsegg'];
-var mobil = tipeMobil[2];
+var mobil = tipeMobil[4];
 
 //Fungsi untuk memilih mobil
-function selectCar() {
+function pilihMobil() {
     if (namaPlayer === '') {
         console.log('Silahkan Isi Nama Anda Untuk Melanjutkan Permainan');
     } else {
         if (mobil === tipeMobil[0]) {
             console.log('Selamat Datang ' + namaPlayer + ' di Game Cars Racing');
-            console.log('Anda memilih mobil BMW sebagai mobil anda');
+            console.log('Anda memilih ' + tipeMobil[0] + ' sebagai mobil anda');
         } else {
             if (mobil === tipeMobil[1]) {
                 console.log('Selamat Datang ' + namaPlayer + ' di Game Cars Racing');
-                console.log('Anda memilih mobil Lamborghini sebagai mobil anda');
+                console.log('Anda memilih ' + tipeMobil[1] + ' sebagai mobil anda');
             } else {
                 if (mobil === tipeMobil[2]) {
                     console.log('Selamat Datang ' + namaPlayer + ' di Game Cars Racing');
-                    console.log('Anda memilih mobil Ferrari sebagai mobil anda');
+                    console.log('Anda memilih ' + tipeMobil[2] + ' sebagai mobil anda');
                 } else {
                     if (mobil === tipeMobil[3]) {
                         console.log(
                             'Selamat Datang ' + namaPlayer + ' di Game Cars Racing'
                         );
-                        console.log('Anda memilih mobil Tesla sebagai mobil anda');
+                        console.log('Anda memilih ' + tipeMobil[3] + ' sebagai mobil anda');
                     } else {
                         if (mobil === tipeMobil[4]) {
                             console.log(
                                 'Selamat Datang ' + namaPlayer + ' di Game Cars Racing'
                             );
-                            console.log('Anda memilih mobil Koenigsegg sebagai mobil anda');
+                            console.log(
+                                'Anda memilih ' + tipeMobil[4] + ' sebagai mobil anda'
+                            );
                             console.log('');
                         }
                     }
@@ -39,29 +41,29 @@ function selectCar() {
         }
     }
 }
-selectCar();
+pilihMobil();
 
 //Fungsi untuk penambahan kecepatan setiap mobil
-function calculateKecepatan(kecepatan, mobil) {
+function menghitungKecepatan(kecepatan, mobil) {
     switch (mobil) {
         //penambahan kecepatan mobil BMW secara random dari 1 - 5
-        case 'BMW':
+        case tipeMobil[0]:
             kecepatan = kecepatan + Math.floor(Math.random() * 5 + 1);
             break;
             //penambahan kecepatan mobil Lamborghini secara random dari 5 - 10
-        case 'Lamborghini':
+        case tipeMobil[1]:
             kecepatan = kecepatan + Math.floor(Math.random() * 10 + 5);
             break;
             //penambahan kecepatan mobil Ferrari secara random dari 5 - 10
-        case 'Ferrari':
+        case tipeMobil[2]:
             kecepatan = kecepatan + Math.floor(Math.random() * 15 + 7);
             break;
             //penambahan kecepatan mobil Tesla secara random dari 5 - 10
-        case 'Tesla':
+        case tipeMobil[3]:
             kecepatan = kecepatan + Math.floor(Math.random() * 20 + 9);
             break;
             //penambahan kecepatan mobil Koenigsegg secara random dari 5 - 10
-        case 'Koenigsegg':
+        case tipeMobil[4]:
             kecepatan = kecepatan + Math.floor(Math.random() * 25 + 12);
             break;
             //penambahan kecepatan mobil Computer secara random dari 5 - 10
@@ -86,14 +88,14 @@ var waktuTempuhPlayer = 0;
 var waktuTempuhCom = 0;
 
 //Fungsi memulai game
-function startGame() {
+function mulaiPermainan() {
     while (jarakTempuhTotalPlayer < finish || jarakTempuhTotalCom < finish) {
-        vEndPlayer = calculateKecepatan(kecepatan, mobil);
+        vEndPlayer = menghitungKecepatan(kecepatan, mobil);
         //Kecepatan akhir player setelah ditambahkan kecepatannya sesuai dengan mobil yang dipilih
-        vEndCom = calculateKecepatan(kecepatan);
+        vEndCom = menghitungKecepatan(kecepatan);
         //Kecepatan akhir com player setelah ditambahkan kecepatannya sesuai dengan default level
         jarakTempuh = 0.5 * (vEndPlayer + vStartPlayer);
-        //Jarak tempuh player per detik yang dihitung dengan Gerak Lurus Berubah Beraturan 
+        //Jarak tempuh player per detik yang dihitung dengan Gerak Lurus Berubah Beraturan
         jarakTempuhTotalPlayer += jarakTempuh; //Akumulasi jarak tempuh player dari detik nol sampai mencapai finish
         jarakTempuh = 0.5 * (vEndCom + vStartCom);
         //Jarak tempuh com player per detik yang dihitung dengan Gerak Lurus Berubah Beraturan
@@ -103,19 +105,19 @@ function startGame() {
         var waktuPlayer = waktuTempuhPlayer.toFixed(2);
         waktuTempuhCom = jarakTempuhTotalCom / vEndCom;
         var waktuCom = waktuTempuhCom.toFixed(2);
-        console.log('Waktu tempuh ' + namaPlayer + ': ' + waktuPlayer);
+        console.log('Waktu tempuh ' + namaPlayer + ': ' + waktuPlayer + ' detik');
         console.log('kecepatan Player: ' + vEndPlayer);
-        console.log('Jarak tempuh komputer: ' + waktuCom);
-        console.log('kecepatan Com Player: ' + vEndCom);
+        console.log('Jarak tempuh komputer: ' + waktuCom + ' detik');
+        console.log('kecepatan Komputer Player: ' + vEndCom);
         console.log('');
         vStartPlayer = vEndPlayer;
         vStartCom = vEndCom;
     }
     if (waktuTempuhPlayer < waktuTempuhCom) {
-        console.log('You Won!!!');
+        console.log('Congratulation,You Won!!!');
     } else {
-        console.log('You Loose!!!');
+        console.log('Sorry, You Loose!!!');
     }
     console.log('Mobil yang digunakan adalah mobil ' + mobil);
 }
-startGame();
+mulaiPermainan();
